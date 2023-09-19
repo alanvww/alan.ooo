394d0fe95e975706fb461ffe71944395bce23599
Alan Ren <10086000+alanvww@users.noreply.github.com>
2023-09-19 19:06:04 -0400

    Fix styling

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,6 +6,7 @@ import HeroSvg from './icons/HeroSvg';
 import dynamic from 'next/dynamic';
 
 import { IsClientCtxProvider } from './utilities/is-client-ctx';
+import Link from 'next/link';
 
 const HydraCanvas = dynamic(() => import('./components/HydraCanvas'), {
 	ssr: false,
@@ -17,11 +18,20 @@ export default async function Home() {
 	return (
 		<IsClientCtxProvider>
 			<HydraCanvas />
-			<main className=" z-10  mx-auto  w-screen h-screen left-0 border-double border-[30px] border-white mix-blend-exclusion">
-				<div className="lg:px-16 px-6 text-4xl md:text-9xl">
-					<h1 className="font-extrabold text-white z-[10]">About Me</h1>
-					<h1 className="font-extrabold text-white z-[10]">Prosjects</h1>
-					<h1 className="font-extrabold text-white z-[10]">Contact Me</h1>
+			<main className=" z-10  mx-auto  flex md:flex-row flex-col items-center justify-center w-screen h-screen left-0 border-double border-8 md:border-[3rem] border-white mix-blend-exclusion">
+				<h1 className="lg:px-16 px-6  text-4xl md:text-[10rem] font-extrabold">
+					Alan Ren
+				</h1>
+				<div className="my-4 text-center md:text-left flex flex-col gap-y-5 lg:px-16 px-6 text-2xl md:text-9xl font-extrabold text-white z-[10]">
+					<Link href="/about" className="hover:underline">
+						About
+					</Link>
+					<Link href="/projects" className="hover:underline">
+						Projects
+					</Link>
+					<Link href="" className="hover:underline">
+						Contact
+					</Link>
 				</div>
 			</main>
 		</IsClientCtxProvider>
