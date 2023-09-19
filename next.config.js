5f0b92ba744b8b4c63a4ee85cd67b56103b01d26
Alan Ren <10086000+alanvww@users.noreply.github.com>
2023-09-19 00:49:30 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
 			},
 		],
 	},
+	typescript: {
+		// Set this to false if you want production builds to abort if there's type errors
+		ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
+	},
+	eslint: {
+		/// Set this to false if you want production builds to abort if there's lint errors
+		ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
+	},
 };
 
 module.exports = nextConfig;
