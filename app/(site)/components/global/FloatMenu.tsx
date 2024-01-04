0c2b34c1497f3becfe3b60d91076f28b3e81c232
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-01-03 22:33:54 -0500

    Update metadata

--- app/(site)/components/global/FloatMenu.tsx
+++ app/(site)/components/global/FloatMenu.tsx
@@ -3,12 +3,15 @@
 import Link from 'next/link';
 import { BiBracket, BiBriefcase, BiAtom } from 'react-icons/bi';
 
+import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 
 export default function FloatMenu() {
 	const pathname = usePathname();
+	const [isExpanded, setIsExpanded] = useState(false); // State for expandable section
+
 	return (
-		<nav className="flex w-full fixed left-0 justify-center bottom-5  md:bottom-16	 text-sm">
+		<nav className=" flex w-full fixed left-0 justify-center bottom-5  md:bottom-16	 text-sm">
 			<section className="rounded-3xl bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-theme-green via-gradient-via to-theme-purple p-px">
 				<ul className=" flex items-center pace gap-x-4 md:gap-x-8 px-8 md:px-16 py-3 rounded-3xl bg-gray-dark transition-all">
 					<li
@@ -51,6 +54,18 @@ export default function FloatMenu() {
 							<BiAtom className="md:inline my-auto mx-1 text-xl" />
 							<span className="align-middle">Projects</span>
 						</Link>
+					</li>
+
+					<li className="md:px-10 px-1 py-2 rounded-xl hover:bg-white hover:text-black duration-300">
+						<button onClick={() => setIsExpanded(!isExpanded)} className="flex">
+							<span className="align-middle">More</span>
+						</button>
+						{isExpanded && (
+							<div className="absolute bg-white p-4 rounded shadow-lg z-10">
+								{/* Content of your popup */}
+								<p>Popup content here...</p>
+							</div>
+						)}
 					</li>
 				</ul>
 			</section>
--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,5 +1,7 @@
 import '../globals.css';
 import type { Metadata } from 'next';
+import type {} from 'next';
+
 import { Inter } from 'next/font/google';
 import Navbar from './components/global/Navbar';
 import Footer from './components/global/Footer';
@@ -10,11 +12,31 @@ import NewCursor from './components/global/NewCursor';
 // const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+	generator: 'Next.js',
+	applicationName: `Portfolio - Alan Ren`,
+	referrer: 'origin-when-cross-origin',
+	keywords: ['Personal Website', 'Portfolio'],
+	authors: [{ name: 'Alan Ren' }],
+	creator: 'Alan Ren',
+	formatDetection: {
+		email: true,
+		address: false,
+		telephone: true,
+	},
 	title: 'Portfolio - Alan Ren',
 	metadataBase: new URL('https://alan.ooo'),
 	description: 'A personal portfolio built with Next.js by Alan Ren',
 	openGraph: {
-		images: './icons/icon-512x512.png',
+		images: '../opengraph-image.jpg',
+	},
+	icons: {
+		icon: './icons/logo.png',
+		shortcut: './icons/shortcut_logo.png',
+		apple: './icons/apple-touch-icon.png',
+		other: {
+			rel: 'apple-touch-icon-precomposed',
+			url: './icons/apple-touch-icon.png',
+		},
 	},
 };
 
@@ -25,10 +47,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<Header></Header>
 			<body className={`w-screen min-h-full text-white bg-black `}>
 				<NewCursor />
-				<div className="">{children}</div>
+				{children}
 			</body>
 		</html>
 	);
