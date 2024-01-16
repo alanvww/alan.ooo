400b2720c8790ba9836ae6a52419b3492dcdf452
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-01-16 15:40:51 -0500

    fix index metadata

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -9,8 +9,34 @@ import { IsClientCtxProvider } from './utilities/is-client-ctx';
 import NewCursor from './components/global/NewCursor';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-// const inter = Inter({ subsets: ['latin'] });
-
+export const metadata: Metadata = {
+	generator: 'Next.js',
+	applicationName: `Portfolio - Alan Ren`,
+	referrer: 'origin-when-cross-origin',
+	keywords: ['Personal Website', 'Portfolio'],
+	authors: [{ name: 'Alan Ren' }],
+	creator: 'Alan Ren',
+	formatDetection: {
+		email: true,
+		address: false,
+		telephone: true,
+	},
+	title: 'Portfolio - Alan Ren',
+	//metadataBase: new URL('https://alan.ooo'),
+	description: 'A personal portfolio built with Next.js by Alan Ren',
+	openGraph: {
+		images: 'opengraph-image.jpg',
+	},
+	icons: {
+		icon: 'favicon.ico',
+		shortcut: 'android-chrome-512x512.png',
+		apple: 'apple-touch-icon.png',
+		other: {
+			rel: 'apple-touch-icon-precomposed',
+			url: 'android-chrome-512x512.png',
+		},
+	},
+};
 export default function RootLayout({
 	children,
 }: {
