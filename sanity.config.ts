03ed0ca799e8cec8b68ddf1a27a7171d560287e0
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-01-18 15:57:42 -0500

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'sanity';
-import { deskTool } from 'sanity/desk';
+import { structureTool } from "sanity/structure";
 import { visionTool } from '@sanity/vision';
 import { copyPastePlugin } from '@superside-oss/sanity-plugin-copy-paste';
 import { media } from 'sanity-plugin-media';
@@ -14,7 +14,7 @@ export default defineConfig({
 	dataset: 'production',
 	basePath: '/studio',
 
-	plugins: [deskTool(), visionTool(), copyPastePlugin(), media()],
+	plugins: [structureTool(), visionTool(), copyPastePlugin(), media()],
 
 	schema: {
 		types: schemaTypes,
