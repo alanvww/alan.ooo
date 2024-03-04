7bb5319d174d228adf54dfa5199b0f01c0678dad
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-03-03 20:14:26 -0500

    Update ClientPlayer.tsx

--- app/(site)/components/shared/ClientPlayer.tsx
+++ app/(site)/components/shared/ClientPlayer.tsx
@@ -3,11 +3,11 @@ import dynamic from 'next/dynamic';
 import React from 'react';
 import { ReactPlayerProps } from 'react-player';
 
-const ReactPlayer = dynamic(() => import('react-player'), {
+const ReactPlayer = dynamic(() => import('react-player/lazy'), {
 	ssr: false,
 });
 
-const ClientSidePlayer = (props: any) => {
+const ClientSidePlayer = (props: ReactPlayerProps) => {
 	return <ReactPlayer {...props} />;
 };
 
