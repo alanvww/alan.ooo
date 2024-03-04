1130de19f7b711dd49b7ea93bdb733d05d72cb36
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-03-03 20:34:51 -0500

    Add layzyload and conent policy

--- app/(site)/components/shared/ClientPlayer.tsx
+++ app/(site)/components/shared/ClientPlayer.tsx
@@ -1,6 +1,6 @@
 'use client';
 import dynamic from 'next/dynamic';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { ReactPlayerProps } from 'react-player';
 
 const ReactPlayer = dynamic(() => import('react-player/lazy'), {
@@ -8,7 +8,11 @@ const ReactPlayer = dynamic(() => import('react-player/lazy'), {
 });
 
 const ClientSidePlayer = (props: ReactPlayerProps) => {
-	return <ReactPlayer {...props} />;
+	return (
+		<Suspense>
+			<ReactPlayer {...props} />
+		</Suspense>
+	);
 };
 
 export default ClientSidePlayer;
