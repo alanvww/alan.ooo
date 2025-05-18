e518edfff8f77b94f0116ff14f2034b73c583e9c
alanvww <10086000+alanvww@users.noreply.github.com>
2025-05-17 23:41:48 -0400

    Try add Sanity Manifest
    Added manifest

---
+++ app/(studio)/studio/[[...tool]]/page.tsx
@@ -0,0 +1,12 @@
+// ./src/app/studio/[[...tool]]/page.tsx
+
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../../sanity.config'
+
+export const dynamic = 'force-static'
+
+export { metadata, viewport } from 'next-sanity/studio'
+
+export default function StudioPage() {
+	return <NextStudio config={config} />
+}
